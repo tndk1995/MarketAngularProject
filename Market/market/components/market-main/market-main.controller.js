6dbe1042d4ff13272326aa1026921fafe86366b0
KhoaTran <tndk1995@gmail.com>
2017-03-06 00:18:06 +0700

    v2

--- Market/market/components/market-main/market-main.controller.js
+++ Market/market/components/market-main/market-main.controller.js
@@ -2,8 +2,13 @@ angular.module('marketMain')
     .controller('marketMainController');
 
 function marketMainController($scope, $http) {
+    $scope.type = '';
     $http.get('market/components/cameras/cameras.json').then(function(response) {
         $scope.cameras = response.data;
     });
-    $scope.selectType = '';
+    $scope.selectType = selectType;
+
+    function selectType(type) {
+        $scope.type = type;
+    }
 }
