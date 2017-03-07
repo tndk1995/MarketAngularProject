842f231e8bb303135b5c836ce02ad00106dd72b6
KhoaTran <tndk1995@gmail.com>
2017-03-08 00:32:57 +0700

    beta v1 -Tim them src di nha

--- Market/market/components/market-detail/market-detail.controller.js
+++ Market/market/components/market-detail/market-detail.controller.js
@@ -2,7 +2,12 @@ angular.module("marketDetail")
     .controller("marketDetailController", marketDetailController);
 
 function marketDetailController($scope, $http, $stateParams) {
+    $scope.setPreview = function(imgUrl) {
+        $scope.imgPreview = imgUrl;
+    };
     $http.get('market/components/cameras/' + $stateParams.id + '.json').then(function(response) {
         $scope.camera = response.data;
+        $scope.imgPreview = $scope.camera.images[0];
     });
+
 }
